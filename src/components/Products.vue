869260e786aba43cbd8a6dca6fd82c83aa41144a
Lazola2 <lazolamakubalo271@gmail.com>
2022-12-13 16:37:36 +0200

    sdsd

---
+++ src/components/Products.vue
@@ -0,0 +1,44 @@
+<template>
+    <div v-for="item in items" :key="item">
+        <div class="card mb-5 text-white" style="width: 18rem;">
+            <img :src=item.imageLink class="card-img-top" :alt="item.brand" loading="lazy">
+            <div class="card-body">
+                <div class="name-and-price d-flex justify-content-between">
+                    <h5 class="card-title">{{item.brand}}</h5>
+                    <h5 class="card-title">R{{item.price}}</h5>
+                </div>
+                
+                <p class="card-text text-center">{{item.specifications.toString()}}</p>
+                <div class="button-holder d-flex justify-content-center">
+                    <button class="btn add-btn w-50" v-on:click='greet'>Add</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProductContainer',
+    data() {
+        return {
+            items: [
+                {
+                    id: 1,
+                    brand: 'Lenovo',
+                    price: 9000,
+                    specifications: ['Lenovo ThinkPad', 'Intel Core i5 Processor', '8GB RAM', '500GB HDD', 'Windows 10 Pro'],
+                    color: 'Silver',
+                    imageLink: 'https://i.postimg.cc/Jzd7CVzQ/Lenovo-thinkpad.webp'
+                },
+            ]
+        }
+    },
+    methods: {
+        greet: function(event){
+            event.preventDefault();
+            alert('Hello there');
+        }
+    }
+}
+</script>
---
+++ src/views/ProductsView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>Products</h1>
+    <ProductsContainer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ProductsContainer from '@/components/Products.vue'
+export default {
+  components: {
+    ProductsContainer
+  }
+}
+</script>
